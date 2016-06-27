e9c7aadace8ef855aa4a6dd5326a364d6e3b0840
elliottcandrew <luciapells@Slumlordbillionaire.local>
2016-06-27 15:12:20 +0200

    Import new site from my-site

---
+++ assets/js/scripts.js
@@ -0,0 +1,57 @@
+$(window).load(function() {
+
+	$('.col1').each(
+		function()
+		{
+			$(this).jScrollPane();
+			var api = $(this).data('jsp');
+			var throttleTimeout;
+			$(window).bind(
+				'resize',
+				function()
+				{
+					if (!throttleTimeout) {
+						throttleTimeout = setTimeout(
+							function()
+							{
+								api.reinitialise();
+								throttleTimeout = null;
+							},
+							50
+						);
+					}
+				}
+			);
+		}
+	);
+
+if ($('.container').css('display') == 'block') {
+
+	$('.container').each(
+		function()
+		{
+			$(this).jScrollPane();
+			var api = $(this).data('jsp');
+			var throttleTimeout;
+			$(window).bind(
+				'resize',
+				function()
+				{
+					if (!throttleTimeout) {
+						throttleTimeout = setTimeout(
+							function()
+							{
+								api.reinitialise();
+								throttleTimeout = null;
+							},
+							50
+						);
+					}
+				}
+			);
+		}
+	);
+
+	}
+
+});
